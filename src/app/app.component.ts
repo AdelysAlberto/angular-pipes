37b046460347bae7056d3063a4cfd7326443698f
Adelys Belen <alberto.belen@fluxit.com.ar>
2020-03-07 18:18:04 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: "app-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'angular-pipes';
+	title = "Pipes";
+	nombre: string = "Capitan America";
+	arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+	PI: number = Math.PI;
+	percentage: number = 0.234;
+	salary: number = 65500.5;
 }
